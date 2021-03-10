c500e353b3a23c6b61dc9c5c5a60cb8024fff1e6
ALStoilkov <antonistoilkov@gmail.com>
2021-03-10 15:46:34 +0000

    1 and 2 ex done

--- index.js
+++ index.js
@@ -28,3 +28,90 @@ let secondimages = [];
 // ${secindimages}`);
 
 // const loadFirstImages = () => {};
+
+let returnCard = (url) => {
+  return `<div class="col-md-4">
+    <div class="card mb-4 shadow-sm">
+      <img src="${url}"/>
+      <div class="card-body">
+        <p class="card-text">
+          This is a wider card with supporting text below as a natural
+          lead-in to additional content. This content is a little bit
+          longer.
+        </p>
+        <div class="d-flex justify-content-between align-items-center">
+          <div class="btn-group">
+            <button type="button" class="btn btn-sm btn-outline-secondary">
+              View
+            </button>
+            <button type="button" class="btn btn-sm btn-outline-secondary">
+              Edit
+            </button>
+          </div>
+          <small class="text-muted">9 mins</small>
+        </div>
+      </div>
+    </div>
+  </div>
+ `;
+};
+
+let returnCardSecond = (url) => {
+  return `<div class="col-md-4">
+      <div class="card mb-4 shadow-sm">
+        <img src="${url}"/>
+        <div class="card-body">
+          <p class="card-text">
+            This is a wider card with supporting text below as a natural
+            lead-in to additional content. This content is a little bit
+            longer.
+          </p>
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="btn-group">
+              <button type="button" class="btn btn-sm btn-outline-secondary">
+                View
+              </button>
+              <button type="button" class="btn btn-sm btn-outline-secondary">
+                Edit
+              </button>
+            </div>
+            <small class="text-muted">9 mins</small>
+          </div>
+        </div>
+      </div>
+    </div>
+   `;
+};
+
+const createCards = () => {
+  const container = document.querySelector(".firstImageSection");
+  container.innerHTML = "";
+  firstimages.forEach((image) => {
+    container.innerHTML += returnCard(image.url);
+  });
+};
+
+const createSecondaryCards = () => {
+  const container = document.querySelector(".secondImageSection");
+  container.innerHTML = "";
+  secondimages.forEach((image) => {
+    container.innerHTML += returnCardSecond(image.url);
+  });
+};
+
+window.onload = function () {
+  const firstimagespromis = fetch(
+    "http://www.splashbase.co/api/v1/images/search?query=laptop"
+  ).then((response) => response.json());
+  const secondary = fetch(
+    "http://www.splashbase.co/api/v1/images/search?query=camera"
+  ).then((response) => response.json());
+  Promise.all([firstimagespromis, secondary])
+    .then((responses) => {
+      firstimages = responses[0].images;
+      console.log(firstimages);
+      secondimages = responses[1].images;
+      console.log(secondimages);
+    })
+    .catch((err) => console.log(err));
+};
