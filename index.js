1f2110be1f8c859b5ff6cd41d66502fb2682aa25
ALStoilkov <antonistoilkov@gmail.com>
2021-03-10 14:49:25 +0000

    linked js

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-let firstimages = []
-let secindimages = []
-window.onload() = function() {  
-  const firstimagespromis = fetch("http://www.splashbase.co/api/v1/images/search?query=your").then(response => response.json())
-  const secondary = fetch("http://www.splashbase.co/api/v1/images/search?query=your").then(response => response.json())
-  Promise.all
-}
+let firstimages = [];
+let secondimages = [];
+// window.onload = function () {
+//   const firstimagespromis = fetch(
+//     "https://striveschool-api.herokuapp.com/api/search?q=eminem"
+//   ).then((response) => response.json());
+//   const secondary = fetch(
+//     "https://striveschool-api.herokuapp.com/api/search?q=metallica"
+//   ).then((response) => response.json());
+//   Promise.all([firstimagespromis, secondary])
+//     .then((responses) => console.log(responses))
+//     .then((responses) => {
+//       firstimages = responses[0].data[0].album.cover_medium;
+//       console.log(firstimages);
+//     })
+//     .catch((err) => console.log(err));
+// };
+
+// window.onload = () => {
+//   const tablePromise = fetch(
+//     "http://www.splashbase.co/api/v1/images/search?query=tables"
+//   )
+//     .then((response) => response.json())
+
+// };
+
+// console.log(`${firstimages}
+// ${secindimages}`);
+
+// const loadFirstImages = () => {};
